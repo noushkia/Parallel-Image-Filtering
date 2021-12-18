bc99672cf27b9c8c0c71e53d4447f8ce59625a80
Kianoush <kianoosharshi@gmail.com>
2021-12-18 18:42:03 +0330

    Add all filters and serial code

--- Makefile
+++ Makefile
@@ -3,17 +3,14 @@ STD=-std=c++11
 CF=$(STD)
 
 
-ImageFilters.out: readImg.o image.o blur.o
-	$(CC) $(CF) -o ImageFilters.out readImg.o image.o blur.o
+ImageFilters.out: readImg.o image.o
+	$(CC) $(CF) -o ImageFilters.out readImg.o image.o
 
-$(BUILD_DIR)/readImg.o: readImg.cpp image.hpp blur.hpp typedefs.hpp
+$(BUILD_DIR)/readImg.o: readImg.cpp image.hpp typedefs.hpp
 	$(CC) $(CF) -c readImg.cpp -o readImg.o
 
 $(BUILD_DIR)/image.o: image.cpp image.hpp typedefs.hpp
 	$(CC) $(CF) -c image.cpp -o image.o
-
-$(BUILD_DIR)/blur.o: blur.cpp blur.hpp typedefs.hpp
-	$(CC) $(CF) -c blur.cpp -o blur.o
 
 .PHONY: clean
 clean:
--- readImg.cpp
+++ readImg.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 #include <unistd.h>
 #include <fstream>
+#include <chrono>
+#include <math.h>
+#include <algorithm>
 
-#include "image.hpp"
 #include "typedefs.hpp"
-#include "blur.hpp"
+#include "image.hpp"
 
 using std::cout;
 using std::endl;
 using std::ifstream;
 using std::ofstream;
 using namespace std;
+using namespace std::chrono;
 
 #pragma pack(1)
 #pragma once
@@ -36,7 +39,7 @@ typedef struct tagBITMAPINFOHEADER
   WORD biPlanes;
   WORD biBitCount;
   DWORD biCompression;
-  DWORD biSizeImage;
+  DWORD biSizepixels;
   LONG biXPelsPerMeter;
   LONG biYPelsPerMeter;
   DWORD biClrUsed;
@@ -46,14 +49,22 @@ typedef struct tagBITMAPINFOHEADER
 
 int rows;
 int cols;
-vector<vector<vector<int>>> pixels;
+vector<vector<vector<unsigned char>>> pixels;
+vector<vector<vector<unsigned char>>> real_pixels;
+
+void grayscale();
+void blur(int lvl);
+void sepia();
+void mean();
+void add_X(int width);
 
 void initialize_pixels()
 {
   for (int i = 0; i < rows; i++) {
-    vector<int> pixel(color_pallete, 0);
-    vector<vector<int>> row(cols, pixel);
+    vector<unsigned char> pixel(color_pallete, 0);
+    vector<vector<unsigned char>> row(cols, pixel);
     pixels.push_back(row);
+    real_pixels.push_back(row);
   }
 }
 
@@ -101,16 +112,16 @@ void getPixlesFromBMP24(int end, int rows, int cols, char *fileReadBuffer)
         switch (k)
         {
         case 0:
-          pixels[i][j][Image::RED] = -fileReadBuffer[end - count];
+          real_pixels[i][j][Image::RED] = fileReadBuffer[end - count];
           break;
         case 1:
-          pixels[i][j][Image::GREEN] = -fileReadBuffer[end - count];
+          real_pixels[i][j][Image::GREEN] = fileReadBuffer[end - count];
           break;
         case 2:
-          pixels[i][j][Image::BLUE] = -fileReadBuffer[end - count];
+          real_pixels[i][j][Image::BLUE] = fileReadBuffer[end - count];
           break;
-        count++;
         }
+        count++;
       }
   }
 }
@@ -134,16 +145,16 @@ void writeOutBmp24(char *fileBuffer, const char *nameOfFileToCreate, int bufferS
         switch (k)
         {
         case 0:
-          fileBuffer[bufferSize - count] = char(pixels[i][j][Image::RED]);
+          fileBuffer[bufferSize - count] = pixels[i][j][Image::RED];
           break;
         case 1:
-          fileBuffer[bufferSize - count] = char(pixels[i][j][Image::GREEN]);
+          fileBuffer[bufferSize - count] = pixels[i][j][Image::GREEN];
           break;
         case 2:
-          fileBuffer[bufferSize - count] = char(pixels[i][j][Image::BLUE]);
+          fileBuffer[bufferSize - count] = pixels[i][j][Image::BLUE];
           break;
-        count++;
         }
+        count++;
       }
   }
   write.write(fileBuffer, bufferSize);
@@ -162,11 +173,157 @@ int main(int argc, char *argv[])
 
   initialize_pixels();
 
+  auto start = high_resolution_clock::now();
+
   getPixlesFromBMP24(bufferSize, rows, cols, fileBuffer);
 
-  grayscale(rows, cols, pixels);
+  auto stop = high_resolution_clock::now();  
+  auto duration = duration_cast<microseconds>(stop - start);
+  cout << "Time taken for reading image: " << duration.count() << " ms" << endl;
 
-  writeOutBmp24(fileBuffer, "ut2.bmp", bufferSize);
+  pixels = real_pixels;
+
+  auto total_start = high_resolution_clock::now();
+  start = high_resolution_clock::now();
+
+  blur(1);
+
+  stop = high_resolution_clock::now();
+  duration = duration_cast<microseconds>(stop - start);
+  cout << "Time taken: " << duration.count() << " ms" << endl;
+  
+  writeOutBmp24(fileBuffer, "blur.bmp", bufferSize);
+  real_pixels = pixels;
+  
+  start = high_resolution_clock::now();
+
+  sepia();
+
+  stop = high_resolution_clock::now();
+  duration = duration_cast<microseconds>(stop - start);
+  cout << "Time taken: " << duration.count() << " ms" << endl;
+  
+  writeOutBmp24(fileBuffer, "sepia.bmp", bufferSize);
+  real_pixels = pixels;
+
+  start = high_resolution_clock::now();
+
+  mean();
+
+  stop = high_resolution_clock::now();
+  duration = duration_cast<microseconds>(stop - start);
+  cout << "Time taken: " << duration.count() << " ms" << endl;
+
+  writeOutBmp24(fileBuffer, "mean.bmp", bufferSize);
+  real_pixels = pixels;
+
+  start = high_resolution_clock::now();
+
+  add_X(2);
+
+  stop = high_resolution_clock::now();
+  duration = duration_cast<microseconds>(stop - start);
+  cout << "Time taken: " << duration.count() << " ms" << endl;
+
+  writeOutBmp24(fileBuffer, "add_X.bmp", bufferSize);
+
+  auto total_end = high_resolution_clock::now();
+  duration = duration_cast<microseconds>(total_end - total_start);
+  cout << "Total time taken for applying filters and writing images: " << duration.count() << " ms" << endl;
 
   return 0;
+}
+
+int blur_color(int i, int j, int color, int lvl)
+{
+	int blurred = 0;
+	for (int k = i - lvl; k <= i + lvl && k > 0 && k < rows; k++)
+		for (int l = j - lvl; l <= j + lvl && l > 0 && l < cols; l++)
+			blurred += real_pixels[k][l][color];
+	return blurred / pow(lvl*2+1, 2);
+}
+
+void blur(int lvl)
+{
+  cout << "Initiating blur filter" << endl;
+
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++)
+      for (int k = 0; k < color_pallete; k++) {
+          pixels[i][j][k] = blur_color(i, j, k, lvl);
+      }
+  }
+
+  cout << "Filter successfull" << endl;
+    
+  return;
+}
+
+void sepia()
+{
+    cout << "Initiating sepia filter" << endl;
+
+    for (int i = 0; i < rows; i++)
+      for (int j = 0; j < cols; j++)
+      {
+        pixels[i][j][0] = min(((real_pixels[i][j][0]*0.393) + (real_pixels[i][j][1]*0.769) + (real_pixels[i][j][2]*0.189)), 255.00);
+        pixels[i][j][1] = min(((real_pixels[i][j][0]*0.349) + (real_pixels[i][j][1]*0.686) + (real_pixels[i][j][2]*0.168)), 255.00);
+        pixels[i][j][2] = min(((real_pixels[i][j][0]*0.272) + (real_pixels[i][j][1]*0.534) + (real_pixels[i][j][2]*0.131)), 255.00);
+      }
+
+    cout << "Filter successfull" << endl;
+    return;
+}
+
+vector<unsigned char> get_total_avg()
+{
+    vector<unsigned char> avg(color_pallete, 0);
+
+    for (int i = 0; i < rows; i++)
+		for (int j = 0; j < cols; j++)
+            for (int k = 0; k < color_pallete; k++)
+                avg[k] += real_pixels[i][j][k];
+
+    for (int i = 0; i < color_pallete; i++)
+        avg[i] /= (rows*cols);
+
+    return avg;
+}
+
+void mean()
+{
+    cout << "Initiating mean filter" << endl;
+    pixels = real_pixels;
+
+    vector<unsigned char> avg(color_pallete, 0);
+    avg = get_total_avg();
+
+    for (int i = 0; i < rows; i++)
+      for (int j = 0; j < cols; j++)
+              for (int k = 0; k < color_pallete; k++)
+                  pixels[i][j][k] = min((real_pixels[i][j][k]*0.4 + avg[k]*0.6), 255.00);
+
+    cout << "Filter successfull" << endl;
+    return;
+}
+
+void add_X(int width)
+{
+  cout << "Initiating X filter" << endl;
+  pixels = real_pixels;
+
+	for (int i = width; i < rows-width; i++)
+        for (int j = -width; j <= width; j++) {
+            // cout << int(pixels[i+j][i+j][0]) << endl;
+            pixels[i+j][i+j] = {255, 255,  255};
+            pixels[i][i+j] = {255, 255,  255};
+            pixels[i+j][i] = {255, 255,  255};
+            pixels[i+j][cols - 1 - i-j] = {255, 255,  255};
+            pixels[i][cols - 1 - i-j] = {255, 255,  255};
+            pixels[i+j][cols - 1 - i] = {255, 255,  255};
+            // cout << int(pixels[i+j][i+j][0]) << endl;
+        }
+
+    cout << "Filter successfull" << endl;
+    return;
 }
